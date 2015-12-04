bef3321d2adf657e4c8709d7ac80464abcfe1164
a7224492 <314158258@qq.com>
2015-12-04 03:25:19 -0500

    Initial commit

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
-dsada
+function People(name){
+    this.name = name; 
+    this.introduce = function(){
+        console.log('i am '+this.name);
+    }
+}
+
+People.Run = function(){
+    console.log('I can run');
+}
+
+People.prototype.introduceChinese = function(){
+    console.log('prototype');
+}
+
+var people = new People('jiangzhen');
+people.hello = function(){
+    console.log(this.name+'hello');
+};
+people.introduce();
+People.Run();
+people.introduceChinese();
+people.hello();
